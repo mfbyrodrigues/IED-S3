4d39134cb8fd461368a1741d8d17e965ef85831e
M. Fernanda <n.nandcchi@gmail.com>
2025-02-22 09:21:02 -0300

    Update main.py

--- Exemplos em sala/01 - 19.02/main.py
+++ Exemplos em sala/01 - 19.02/main.py
@@ -1,7 +1,7 @@
-# declaração de variável
+# Declaração de variável
 variavel = 0
 
-# declaração da função
+# Declaração da função
 def mostrar():
     print(variavel)
 
@@ -13,34 +13,34 @@ def outra_funcao():
 
 mostrar()
 
-# estruturas condicionais:
+# Estruturas condicionais:
 if (variavel < 1): # se
     print('ok')
 
-elif (variavel > 5): #se não, se
+elif (variavel > 5): # Se não, se
     print(bigger)
 
-else: # se não
+else: # Se não
     print('not ok')
 
-# estrutura de repetição:
+# Estrutura de repetição:
 def loop():
     for i in range(10):
         print(i)
 
-# listas:
+# Listas:
 lista_de_fruta = ['siriguela', 'pinha', 'melancia', 1, True, 3.1415, (), [], {}]
 
 print(lista_de_frutas[0])
 
-print(lista_de_frutas[9]) # erro 'Out Of Range'
+print(lista_de_frutas[9]) # Erro 'Out Of Range'
 
-lista_de_fruta.index('melancia') # várias funções catalogadas para se usar
+lista_de_fruta.index('melancia') # Várias funções catalogadas para se usar
 print (index_melancia) # Mostra o index da posição do elemento
 
-print(lista_de_fruta.append('manga')) # adiciona o elemento bem no final
+print(lista_de_fruta.append('manga')) # Adiciona o elemento bem no final
 
-# outra maneira de percorrer:
+# Outra maneira de percorrer:
 
 for fruta in range(len(lista_de_frutas)):
     print(fruta)
