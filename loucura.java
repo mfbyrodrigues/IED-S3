0bfb085a8c7e59cf93e5f87b5b93b329d91c838f
M. Fernanda <n.nandcchi@gmail.com>
2025-03-14 11:34:17 -0300

    Create loucura.java

---
+++ loucura.java
@@ -0,0 +1,65 @@
+// Crie 3 vetores de tipos diferentes.
+// Em cada vetor crie o método de adicionar, remover por índice (diminuindo o tamanho), remover por índice (mantendo o tamanho), exibir e tamanho.
+
+// 1. Vetor inteiro 
+// 2. Vetor string 
+// 3. Vetor float
+
+public class Vetor {
+
+    private int VetorInteiro[];
+    private String VetorString[];
+    private float VetorFloat[];
+
+    private int TamanhoVetor;
+    private int CapacidadeVetor;
+
+
+    // VETOR DO TIPO INTEIRO: 
+    // Adicionar elementos:
+    public void AdicionarVetorInteiro () {
+        
+    }
+
+    // Remover por índice (diminuindo o tamanho):
+    public void RemoverPorIndiceVetorInteiroDT () {
+
+    }
+
+    // Remover por índice (mantendo o tamanho):
+    public void RemoverPorIndiceVetorInteiroMT () {
+
+    }
+
+    // Exibir elementos:
+    public void ExibirElementosVetorInteiro () {
+
+        System.out.println ("Vetor inteiro: ");
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println(elementos[i] + " ");
+        }
+    
+    // Mostrar tamanho:
+    public void MostrarTamanhoVetorInteiro () {
+
+    }
+    
+
+
+    // Vetor do tipo string
+    public void VetorString () {
+
+    }
+
+
+
+    // Vetor do tipo float
+    public void VetorFloat () {
+
+    }
+
+    public static void main (String [] args) {
+         
+    }
+}
